6f7c18aebd2e89f6d7099b9a949352b4da5ead6a
Junaid Ahmad Mir <79140912+mirjunaid@users.noreply.github.com>
2021-02-17 21:54:58 +0530

    Add files via upload
    Java program to check whether a number is even or odd.

---
+++ Main.java
@@ -0,0 +1,16 @@
+package com.evenOdd;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int a ,result;
+        Scanner in=newScanner(System.in);
+        System.out.println("enter the number");
+        a=in.nextint();
+
+    }
+}
